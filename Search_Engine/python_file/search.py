9292604c258ac21fceae4420473dd0509957eab7
sohamsavalapurkar <soham1999@gmail.com>
2022-04-25 17:02:40 -0500

    minor changes

--- Search_Engine/python_file/search.py
+++ Search_Engine/python_file/search.py
@@ -8,15 +8,17 @@ except ImportError:
     print("No module named 'google' found")
  
 # to search
-query = sys.argv[1]
+query = ' '.join(sys.argv[1:])
 for j in search(query, tld="co.in", num=10, stop=10, pause=2):
-    reqs = requests.get(j)
-    soup = BeautifulSoup(reqs.text, 'html.parser')
-    for title in soup.find_all('title'):
-        temp = title.get_text()
-        break
-    if temp == '' or temp == '403 Forbidden':
-        print(f'{j},{j}')
-    else:
-        print(f'{temp},{j}')
-
+    try:
+        reqs = requests.get(j)
+        soup = BeautifulSoup(reqs.text, 'html.parser')
+        for title in soup.find_all('title'):
+            temp = title.get_text()
+            break
+        if temp == '' or temp == '403 Forbidden':
+            print(f'{j},{j}')
+        else:
+            print(f'{temp},{j}')
+    except:
+        pass
